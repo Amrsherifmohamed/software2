520a96aec3d6e4cd4424c69b01018d947040ec53
amr sherif <amrshrif53@gmail.com>
2019-03-29 09:53:57 -0700

    project

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+using Jop_Offers_Website.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Jop_Offers_Website.Controllers
+{
+    public class HomeController : Controller
+    {
+       
+}
---
+++ Controllers/RolesController.cs
@@ -0,0 +1,17 @@
+using Jop_Offers_Website.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Jop_Offers_Website.Controllers
+{
+    
+    public class RolesController : Controller
+    {
+	
+	
+    }
+}
