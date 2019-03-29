7a3e71071f0334c7257bc912ffd171fe9f58811e
amr sherif <amrshrif53@gmail.com>
2019-03-29 12:22:34 -0700

    implementation in search bar

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -14,5 +14,19 @@ namespace Jop_Offers_Website.Controllers
 {
     public class HomeController : Controller
     {
+//search Bar
+        public ActionResult Search() //amr
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Search(string search)
+        {
+            var r = db.Jops.Where(a => a.jopContent.Contains(search)
+            || a.jopTitle.Contains(search) 
+            || a.Category.CategoryName.Contains(search));
+            return View(r.ToList());
+        }
+}
        
 }
