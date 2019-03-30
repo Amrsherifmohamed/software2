c745b4c73db700c348d85c2258c6758b9ac31cfd
hesham <heshamfahmi28@gmail.com>
2019-03-30 14:38:02 -0700

    add login and register in database model

--- Models/AccountViewModels.cs
+++ Models/AccountViewModels.cs
@@ -48,6 +48,51 @@ namespace Jop_Offers_Website.Models
     }
 
     //eeeeeeeeeeeeee
+public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "UserName")]      
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember me?")]
+        public bool RememberMe { get; set; }
+    }
+
+    public class RegisterViewModel
+    {
+        [Required]
+        [DisplayName("UserType")]
+        public string UserType { get; set; }
+
+
+        [Required]
+        [DisplayName("UserName")]
+        public string UserName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+
+
+  
 	public class EditProfileViewModel
     {
 
