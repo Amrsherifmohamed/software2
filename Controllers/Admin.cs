c59f98e32b5af3b24c33c81d77c7f8596993d0ff
amr sherif <amrshrif53@gmail.com>
2019-05-01 08:52:02 -0700

    method for admin

--- Controllers/Admin.cs
+++ Controllers/Admin.cs
@@ -16,6 +16,59 @@ namespace Jop_Offers_Website.Controllers
             var user = db.Users.ToList();
             return View(user);
         }
+    public ActionResult Create()
+        {
+            ViewBag.CategoryId = new SelectList(db.Categories, "Id", "CategoryName");
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]       
+        public ActionResult Create( Jop jop, HttpPostedFileBase file)
+        {
+            string fileName = Path.GetFileNameWithoutExtension(file.FileName);
+            string extension = Path.GetExtension(file.FileName);
+            fileName = fileName + DateTime.Now.ToString("yymmssfff") + extension;
+            jop.jopImg = "~/files/" + fileName;
+
+            fileName = Path.Combine(Server.MapPath("~/files/"), fileName);
+            file.SaveAs(fileName);
+
+            using (ApplicationDbContext db = new ApplicationDbContext())
+            {
+                jop.UserId = User.Identity.GetUserId();
+                db.Jops.Add(jop);
+                db.SaveChanges();
+                return RedirectToAction("Index","Home");
+            }
+            ModelState.Clear();
+            ViewBag.CategoryId = new SelectList(db.Categories, "Id", "CategoryName", jop.CategoryId);
+            return View(jop);
+
+        }
+
+        GET: Jops
+        public ActionResult Index()
+        {
+            var jops = db.Jops.Include(j => j.Category);
+            return View(jops.ToList());
+        }
+
+        GET: Jops/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                Error Msg
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Jop jop = db.Jops.Find(id);
+            if (jop == null)
+            {
+                return HttpNotFound();
+            }
+            return View(jop);
+        }
         public ActionResult Details(string id)
         {
             if (id == null)
